0af1d4a6c984d506b855609324dd255637494a44
ahareed27 <120029125+ahareed27@users.noreply.github.com>
2026-06-02 08:51:47 +0800

    feat: PurchaseOrderLineView Setup

---
+++ Application.UseCases/Queries/Transaction/Receiving/PurchaseOrder/Mobile/MGetPurchaseOrderLineQry.cs
@@ -0,0 +1,28 @@
+﻿using Application.DataTransferObjects.Transactions.Receiving.NS;
+using Application.DataTransferObjects.Transactions.Receiving.Request;
+using Application.UseCases.Repositories.Integration.Others;
+using Mapster;
+using MediatR;
+
+namespace Application.UseCases.Queries.Transaction.Receiving.PurchaseOrder.Mobile;
+
+public record MGetPurchaseOrderLineQry(PurchaseOrderLineRequestDTO order) : IRequest<IEnumerable<PurchaseOrderLineDTO>>;
+
+public class MGetPurchaseOrderLineQryHandler(
+    INetSuiteApiClientService netSuiteApiClientService)
+    : IRequestHandler<MGetPurchaseOrderLineQry, IEnumerable<PurchaseOrderLineDTO>>
+{
+    public async Task<IEnumerable<PurchaseOrderLineDTO>> Handle(
+        MGetPurchaseOrderLineQry request,
+        CancellationToken cancellationToken)
+    {
+        var parameters = new Dictionary<string, string>
+        {
+            ["tranid"] = request.order.OrderNumber
+        };
+
+        var Data = await netSuiteApiClientService.NetsuiteQuery<PurchaseOrderLineDTO>("NS_PurchaseOrder_Get_Items", parameters);
+
+        return Data.Adapt<IEnumerable<PurchaseOrderLineDTO>>();
+    }
+}
