34273057ce569c9592276f71cf3215cf7e1d0871
tbsirupert <tbsi.rupert@gmail.com>
2026-06-15 15:06:49 +0800

    feat: add location dto, location integration

---
+++ Integration.NS/Implementations/Others/LocationIntegration.cs
@@ -0,0 +1,35 @@
+﻿using Application.DataTransferObjects.Others;
+using Application.UseCases.Repositories.Integration.Others;
+using Integration.NS.Services;
+using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration.NS.Implementations.Others;
+
+public class LocationIntegration(
+    INetSuiteApiClientService netsuiteService,
+    SuiteQLQueryBuilderFactoryService builderFactory) : ILocationIntegration
+{
+    public async Task<(IEnumerable<LocationDTO> data, int count)> GetLocationsAsync(DataGridIntent intent)
+    {
+        var query = builderFactory.Create()
+            .Select(
+                ("id", nameof(LocationDTO.Id)),
+                ("externalId", nameof(LocationDTO.LocationNumber)),
+                ("name", nameof(LocationDTO.Name)),
+                ("BUILTIN.DF(mainaddress)", nameof(LocationDTO.Address)),
+                ("BUILTIN.DF(subsidiary)", nameof(LocationDTO.Subsidiary)),
+                ("subsidiary", nameof(LocationDTO.SubsidiaryId))
+            )
+            .From("location")
+            .WithDatagridIntent(intent)
+            .Build();
+
+        var result = await netsuiteService.ExecuteSuiteQLQuery<LocationDTO>(query.Query, query.Limit, query.Offset);
+        return (result.items, result.totalResults);
+    }
+}
