9ea64a2578234f4d44f8671324330e9811875678
tbsirupert <tbsi.rupert@gmail.com>
2026-06-16 13:01:52 +0800

    feat: vendors

---
+++ Web.BlazorServer/Handlers/Repositories/Others/IVendorHandler.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Entities;
+using Web.BlazorServer.ViewModels.Others;
+
+namespace Web.BlazorServer.Handlers.Repositories.Others;
+
+public interface IVendorHandler
+{
+    Task<(IEnumerable<VendorVM> Data, int Count)> GetVendorsListAsync(DataGridIntent intent);
+}
