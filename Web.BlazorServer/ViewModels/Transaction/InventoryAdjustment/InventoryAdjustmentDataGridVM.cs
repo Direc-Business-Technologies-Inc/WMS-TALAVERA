c566bcecd3f05596fc7551828ea8e1ffad3fa3ac
tbsirupert <tbsi.rupert@gmail.com>
2026-06-18 14:55:37 +0800

    feat: use quick datagrid

--- Web.BlazorServer/ViewModels/Transaction/InventoryAdjustment/InventoryAdjustmentDataGridVM.cs
+++ Web.BlazorServer/ViewModels/Transaction/InventoryAdjustment/InventoryAdjustmentDataGridVM.cs
@@ -1,14 +1,20 @@
-﻿using Web.BlazorServer.ViewModels.Others;
+﻿using Web.BlazorServer.Components.Custom.Utilities;
+using Web.BlazorServer.ViewModels.Others;
 
 namespace Web.BlazorServer.ViewModels.Transaction.InventoryAdjustment;
 
 public class InventoryAdjustmentDataGridVM
 {
+    [QuickDataGridIgnore]
     public int Id { get; set; }
+    [QuickDataGridTitle("Reference Number")]
     public string ReferenceNumber { get; set; } = string.Empty;
     public string Subsidiary { get; set; } = string.Empty;
+    [QuickDataGridTitle("Warehouse")]
     public string Location { get; set; } = string.Empty;
     public string Account { get; set;  } = string.Empty;
+    [QuickDataGridTitle("Remarks")]
     public string Memo { get; set; } = string.Empty;
+    [QuickDataGridTitle("Prepared By")]
     public string PreparedBy { get; set; } = string.Empty;
 }
