ef03a4c13900aa38f606de545e7ab9f6b3c86a15
tbsirupert <tbsi.rupert@gmail.com>
2026-06-24 10:33:51 +0800

    feat: query subsidiaries by handler

--- Integration.NS/Implementations/Others/SubsidiaryIntegration.cs
+++ Integration.NS/Implementations/Others/SubsidiaryIntegration.cs
@@ -2,6 +2,7 @@
 using Application.UseCases.Repositories.Integration.Others;
 using Integration.NS.Services;
 using Shared.Entities;
+using Shared.Libraries.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,26 @@ public class SubsidiaryIntegration(
             .Build();
 
         var result = await netsuiteService.ExecuteSuiteQLQuery<SubsidiaryDTO>(query.Query, query.Limit, query.Offset);
+        return (result.items, result.totalResults);
+    }
+
+    public async Task<(IEnumerable<SubsidiaryDTO> Data, int Count)> GetSubsidiariesByVendorAsync(DataGridIntent intent, int vendorId)
+    {
+        var query = builderFactory.Create()
+            .Select(
+                ("s.id", nameof(SubsidiaryDTO.Id)),
+                ("s.externalid", nameof(SubsidiaryDTO.SubsidiaryNumber)),
+                ("s.BUILTIN.DF(mainaddress)", nameof(SubsidiaryDTO.Address)),
+                ("s.name", nameof(SubsidiaryDTO.Name)),
+                ("s.email", nameof(SubsidiaryDTO.Email))
+            )
+            .From("subsidiary s")
+            .Join("vendorSubsidiaryRelationship vsr", "vsr.subsidiary = s.id")
+            .WithFilters(DataGridFilterUtilities.Equal("vsr.entity", vendorId))
+            .WithDatagridIntent(intent)
+            .Build();
+
+        var result = await query.ExecuteWithPaging<SubsidiaryDTO>(netsuiteService);
         return (result.items, result.totalResults);
     }
 }
