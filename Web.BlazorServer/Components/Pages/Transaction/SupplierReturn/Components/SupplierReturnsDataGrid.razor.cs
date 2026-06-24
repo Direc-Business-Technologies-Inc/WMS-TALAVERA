f111d7ee8ffaccdf3e1729bdbb36a89b2da49fca
tbsirupert <tbsi.rupert@gmail.com>
2026-06-24 11:17:16 +0800

    feat: add status filter feat: add create button

--- Web.BlazorServer/Components/Pages/Transaction/SupplierReturn/Components/SupplierReturnsDataGrid.razor.cs
+++ Web.BlazorServer/Components/Pages/Transaction/SupplierReturn/Components/SupplierReturnsDataGrid.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Shared.Entities;
+using Shared.Libraries.Utilities;
+using Web.BlazorServer.Components.Custom;
 using Web.BlazorServer.Handlers.Repositories.Transaction.SupplierReturn;
 using Web.BlazorServer.ViewModels.Transaction.SupplierReturn;
 
@@ -12,9 +14,32 @@ public partial class SupplierReturnsDataGrid
 
     readonly string ActionGetReturnsToSupplier = "Get Returns to Supplier";
 
+    ReturnStatusVM? FilterStatus = null;
+
+    QuickDataGrid<SupplierReturnDataGridVM> Grid { get; set; } = default!;
+
     async Task<(IEnumerable<SupplierReturnDataGridVM>, int)> LoadDataAsync(DataGridIntent intent)
     {
+        if (FilterStatus is not null)
+        {
+            intent.Filters.Add(
+                DataGridFilterUtilities.Contains(
+                    nameof(SupplierReturnDataGridVM.StatusName),
+                    FilterStatus.Name));
+        }
+
         return await returnHandler.GetReturnsDataGridAsync(intent);
+    }
+    async Task<(IEnumerable<ReturnStatusVM>, int)> StatusProvider(DataGridIntent intent)
+    {
+        return await returnHandler.GetReturnStatuses(intent);
+    }
+
+    async Task FilterSet(ReturnStatusVM? filter)
+    {
+        FilterStatus = filter;
+
+        await Grid.Reload();
     }
 
     async Task OnRowAction(SupplierReturnDataGridVM data)
