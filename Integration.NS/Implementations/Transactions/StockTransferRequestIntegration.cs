6365c9ea58d8d2dcaff0bc4393fe5ab750288632
tbsirupert <tbsi.rupert@gmail.com>
2026-06-24 12:45:29 +0800

    fix: [IIL_063] - add alloted quantity to query

--- Integration.NS/Implementations/Transactions/StockTransferRequestIntegration.cs
+++ Integration.NS/Implementations/Transactions/StockTransferRequestIntegration.cs
@@ -187,7 +187,8 @@ internal class StockTransferRequestIntegration(
                 ("uom.conversionrate", nameof(StockTransferRequestLineNSDTO.UoMRate)),
                 ("BUILTIN.DF(tl.location)", nameof(StockTransferRequestLineNSDTO.Warehouse)),
                 ("item.displayname", nameof(StockTransferRequestLineNSDTO.ItemDescription)),
-                ("iil.quantityonhand", nameof(StockTransferRequestLineNSDTO.QuantityOnHand))
+                ("iil.quantityonhand", nameof(StockTransferRequestLineNSDTO.QuantityOnHand)),
+                ("tl.quantity", nameof(StockTransferRequestLineNSDTO.QuantityAlloted))
             )
             .From("transactionline tl")
             .Join("transaction t", on: "tl.transaction = t.id")
