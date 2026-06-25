27cfc6c7312d8f324163ad2870a8fe7cf21b60d5
tbsirupert <tbsi.rupert@gmail.com>
2026-06-25 13:53:04 +0800

    fix: [IIL_068] - add missing property attributes to stock transfer request lines grid

--- Web.BlazorServer/ViewModels/Others/ItemUnitVM.cs
+++ Web.BlazorServer/ViewModels/Others/ItemUnitVM.cs
@@ -1,9 +1,15 @@
 ﻿namespace Web.BlazorServer.ViewModels.Others;
 
-public class ItemUnitVM
+public class ItemUnitVM : IComparable<ItemUnitVM>
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Abbreviation { get; set; } = string.Empty;
     public decimal ConversionRate { get; set; }
+    public int CompareTo(ItemUnitVM? other)
+    {
+        if (other == null) return 1;
+
+        return Name.CompareTo(other.Name);
+    }
 }
