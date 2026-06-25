385fc733440222e22b6560d0349c9769e4e3c696
tbsirupert <tbsi.rupert@gmail.com>
2026-06-25 17:26:28 +0800

    fix: [IIL_081] - apply filter for positive-valued items

--- Web.BlazorServer/Components/Custom/ItemSelectionDialog.razor.cs
+++ Web.BlazorServer/Components/Custom/ItemSelectionDialog.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Shared.Entities;
 using Web.BlazorServer.ViewModels.Others;
 
 namespace Web.BlazorServer.Components.Custom;
@@ -6,6 +7,7 @@ namespace Web.BlazorServer.Components.Custom;
 partial class ItemSelectionDialog
 {
     [Parameter] public int? Location { get; set; } = null;
+    [Parameter] public List<AppFilterDescriptor> Filters { get; set; } = [];
     async Task OnItemsSelected(List<ItemsVM> items)
     {
        DialogService.Close(items);
