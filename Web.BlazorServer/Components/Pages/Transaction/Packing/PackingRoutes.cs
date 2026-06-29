19f15cbbb7b8d2936ca934c851fa76509dd7135d
LuisBandivas <tbsi.luis@gmail.com>
2026-06-29 17:21:38 +0800

    chore: create delivery navigation button for packing module

--- Web.BlazorServer/Components/Pages/Transaction/Packing/PackingRoutes.cs
+++ Web.BlazorServer/Components/Pages/Transaction/Packing/PackingRoutes.cs
@@ -2,7 +2,7 @@ namespace Web.BlazorServer.Components.Pages.Transaction.Packing;
 
 public static class PackingRoutes
 {
-    public const string Root = "/transactions/inventory/packing";
+    public const string Root = "/transactions/delivery/packing";
     public const string StockTransferRequestView = $"{Root}/stock-transfer-request/view";
     public const string CreateItemReceipt = $"{Root}/create-item-receipt";
     public const string ReturnsView = $"{Root}/returns/view";
