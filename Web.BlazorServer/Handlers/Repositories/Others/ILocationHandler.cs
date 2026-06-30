cfa3d443aa6e78a1c4ef567c2498dec8030249d0
tbsirupert <tbsi.rupert@gmail.com>
2026-06-30 16:45:39 +0800

    feat: add inventory balance to location handler

--- Web.BlazorServer/Handlers/Repositories/Others/ILocationHandler.cs
+++ Web.BlazorServer/Handlers/Repositories/Others/ILocationHandler.cs
@@ -9,5 +9,6 @@ public interface ILocationHandler
     Task<(IEnumerable<LocationVM> Data, int Count)> GetSublocationsOfLocationAsync(DataGridIntent intent, int location);
     Task<(IEnumerable<LocationVM> Data, int Count)> GetLocationsBySubsidiaryAsync(DataGridIntent intent, int subsidiaryId);
     Task<(IEnumerable<LocationBinVM> Data, int Count)> GetLocationBinsAsync(int locationId, DataGridIntent intent);
+    Task<(IEnumerable<InventoryBalanceVM> Data, int Count)> GetLocationInventoryBalanceAsync(int locationId, DataGridIntent intent);
 
 }
