2f451a0710fd76b6d084f4777ef5fa5848c4becd
tbsirupert <tbsi.rupert@gmail.com>
2026-06-30 16:38:27 +0800

    feat: add location queries

--- Application.DataTransferObjects/Others/Inventory/InventoryBalanceDTO.cs
+++ Application.DataTransferObjects/Others/Inventory/InventoryBalanceDTO.cs
@@ -14,4 +14,52 @@ public class InventoryBalanceDTO
     public InventoryStatusDTO? Status { get; set; }
     public decimal QuantityOnHand { get; set; }
     public decimal QuantityCommited { get; set; }
+
+    public int LocationId {
+        get => Location?.Id ?? -1;
+        set
+        {
+            if (Location != null) Location.Id = value;
+        }
+    }
+    public string LocationName
+    {
+        get => Location?.Name ?? string.Empty;
+        set
+        {
+            if (Location != null) Location.Name = value;
+        }
+    }
+
+    public int StatusId {
+        get => Status?.Id ?? -1;
+        set
+        {
+            if (Status != null) Status.Id = value;
+        }
+    }
+    public string StatusName
+    {
+        get => Status?.Name ?? string.Empty;
+        set
+        {
+            if (Status != null) Status.Name = value;
+        }
+    }
+
+    public int BinId {
+        get => Bin?.Id ?? -1;
+        set
+        {
+            if (Bin != null) Bin.Id = value;
+        }
+    }
+    public string BinName
+    {
+        get => Bin?.BinNumber ?? string.Empty;
+        set
+        {
+            if (Bin != null) Bin.BinNumber = value;
+        }
+    }
 }
---
+++ Application.UseCases/Queries/Others/Location/GetLocationBinInventoryBalanceQry.cs
@@ -0,0 +1,31 @@
+﻿using Application.DataTransferObjects.Others.Inventory;
+using Application.UseCases.Repositories.Integration.Others;
+using Mapster;
+using MediatR;
+using Shared.Entities;
+using Shared.Libraries.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Queries.Others.Location;
+
+public record GetLocationBinInventoryBalanceQry(DataGridIntent Intent, int binId) 
+    : IRequest<(IEnumerable<InventoryBalanceDTO>, int)>;
+
+public class GetLocationBinInventoryBalanceQryHandler(
+    IInventoryIntegration integration
+    ) : IRequestHandler<GetLocationBinInventoryBalanceQry, (IEnumerable<InventoryBalanceDTO>, int)>
+{
+    public async Task<(IEnumerable<InventoryBalanceDTO>, int)> Handle(GetLocationBinInventoryBalanceQry request, CancellationToken cancellationToken)
+    {
+        var intent = request.Intent.Adapt<DataGridIntent>();
+        intent.Filters.Add(
+            DataGridFilterUtilities.Equal(nameof(InventoryBalanceDTO.BinId), request.binId)
+        );
+
+        return await integration.GetInventoryBalance(intent);
+    }
+}
