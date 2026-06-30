9a22ab7d4d47bd8d9a64320edc5fb33c2a6ec1ef
tbsirupert <tbsi.rupert@gmail.com>
2026-06-30 16:41:19 +0800

    feat: inventory balance vm, inventory status vm

---
+++ Web.BlazorServer/ViewModels/Others/InventoryBalanceVM.cs
@@ -0,0 +1,14 @@
+﻿using Application.DataTransferObjects.Others;
+using Application.DataTransferObjects.Others.Inventory;
+
+namespace Web.BlazorServer.ViewModels.Others;
+
+public class InventoryBalanceVM
+{
+    public int ItemId { get; set; }
+    public LocationBinVM? Bin { get; set; }
+    public LocationVM? Location { get; set; }
+    public InventoryStatusVM? Status { get; set; }
+    public decimal QuantityOnHand { get; set; }
+    public decimal QuantityCommited { get; set; }
+}
