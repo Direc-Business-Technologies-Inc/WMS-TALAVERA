17cf53cd9d823731d74fd91d847f85379234efaa
tbsirupert <tbsi.rupert@gmail.com>
2026-07-08 13:34:13 +0800

    feat: keep barcodes between dialog,

--- Web.BlazorServer/Components/Pages/Transaction/Receiving/Components/ScanDialog.razor.cs
+++ Web.BlazorServer/Components/Pages/Transaction/Receiving/Components/ScanDialog.razor.cs
@@ -3,66 +3,61 @@ using Microsoft.AspNetCore.Components;
 using Web.BlazorServer.Handlers.Repositories.Transaction.Receiving;
 using Web.BlazorServer.Helpers;
 using Web.BlazorServer.ViewModels.Transaction.Receiving;
+using static Web.BlazorServer.Components.Pages.Transaction.Receiving.Components.ItemReceiptForm;
 
 namespace Web.BlazorServer.Components.Pages.Transaction.Receiving.Components;
 
 public partial class ScanDialog
 {
     [Parameter] public EventCallback<(BarcodeVM, bool)> OnBarcodeAdded { get; set; }
+    [Parameter] public Dictionary<string, BarcodeCollection> Barcodes { get; set; } = new();
     [Inject] IReceivingHandler receivingHandler { get; set; } = default!;
 
     bool IsLoadingData => AppBusyService.IsBusy(ActionGetBarcode);
     bool IsGood = true;
 
     readonly string ActionGetBarcode = "Get Barcode";
-
+    Dictionary<string, BarcodeVM?> barcodeCache = new();
     BarcodeVM barcode = new();
-    Dictionary<string, BarcodeCounter> barcodeBank = new();
-    Dictionary<string, BarcodeVM> barcodeCache = new();
     string HelperString = "";
     public async Task AddBarcodeScan()
     {
+        if (string.IsNullOrEmpty(barcode.Barcode))
+        {
+            HelperString = "Please enter a barcode";
+            return;
+        }
+
         AppBusyService.SetBusy(ActionGetBarcode, true);
         await InvokeAsync(StateHasChanged);
 
         var action = await AppActionFactory.RunLoadingAsync(async () => {
 
-            var data = barcodeCache.TryGetValue(barcode.Barcode, out BarcodeVM value) ?
+            var data = barcodeCache.TryGetValue(barcode.Barcode, out BarcodeVM? value) ?
                 value :
                 await receivingHandler.GetBarcodeData(barcode.Barcode);
 
             barcodeCache[barcode.Barcode] = data;
             if (data is null)
             {
-                HelperString = "Couldn't find data for that barcode";
+                HelperString = $"Couldn't find data for barcode {barcode.Barcode}";
             }
             else
             {
                 try
                 {
                     await OnBarcodeAdded.InvokeAsync((data, IsGood));
-                    if (barcodeBank.ContainsKey(data.Barcode))
-                        barcodeBank[data.Barcode].Count++;
-                    else
-                        barcodeBank.Add(data.Barcode, new BarcodeCounter(data));
-
                     HelperString = string.Empty;
                 }
                 catch (Exception ex) 
                 {
                     HelperString = ex.Message;
                 }
-
             }
+            barcode.Barcode = string.Empty;
         }, ActionGetBarcode);
         AppBusyService.SetBusy(ActionGetBarcode, false);
         await InvokeAsync(StateHasChanged);
-    }
-
-    public class BarcodeCounter(BarcodeVM barcode, int count = 1)
-    {
-        public BarcodeVM Barcode { get; init;  } = barcode;
-        public int Count { get; set; } = count;
     }
 
     public void Close(bool save)
