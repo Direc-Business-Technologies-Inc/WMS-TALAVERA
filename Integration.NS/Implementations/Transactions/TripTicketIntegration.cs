9d1189c1c1c56387dd4c863088a5f034df6ee142
tbsirupert <tbsi.rupert@gmail.com>
2026-07-10 10:18:08 +0800

    fix: use driver name instead of employee code in trip ticket datagrid, view page

--- Integration.NS/Implementations/Transactions/TripTicketIntegration.cs
+++ Integration.NS/Implementations/Transactions/TripTicketIntegration.cs
@@ -70,7 +70,7 @@ public class TripTicketIntegration(
                 ("tt.id", nameof(TripTicketDataGridDTO.NetsuiteTripTicketInternalId)),
                 ("tt.name", nameof(TripTicketDataGridDTO.Name)),
                 ("BUILTIN.DF(tt.custrecord_dbti_destination)", nameof(TripTicketDataGridDTO.Destination)),
-                ("BUILTIN.DF(tt.custrecord_dbti_trt_assigned_driver)", nameof(TripTicketDataGridDTO.Driver)),
+                ("CONCAT(e.firstname, CONCAT(' ', e.lastname))", nameof(TripTicketDataGridDTO.Driver)),
                 ("BUILTIN.DF(tt.custrecord_dbti_trt_origin_location)", nameof(TripTicketDataGridDTO.Location)),
                 ("TO_CHAR(tt.custrecord_dbti_trt_date, 'YYYY-MM-DD\"T\"HH24:MI:SS')", nameof(TripTicketDataGridDTO.TripDate)))
             .From("customrecord_dbti_trip_ticket tt")
@@ -79,6 +79,7 @@ public class TripTicketIntegration(
             on: "tt.id = ttif.custrecord_dbti_ttf_trip_ticket_num")
         .Join(
             "transaction t",
-            on: "ttif.custrecord_dbti_ttf_item_fulfillment_num = t.id");
+            on: "ttif.custrecord_dbti_ttf_item_fulfillment_num = t.id")
+        .LeftJoin("employee e", "e.id = tt.custrecord_dbti_trt_assigned_driver");
     }
 }
