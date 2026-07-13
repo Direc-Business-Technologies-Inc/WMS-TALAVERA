0b13490eb8ad4f64476fef85ad167542c6b9dc1e
tbsirupert <tbsi.rupert@gmail.com>
2026-07-13 10:28:03 +0800

    fix: stock transfer request - remove status in payload

--- Integration.NS/Implementations/Transactions/StockTransferRequestIntegration.cs
+++ Integration.NS/Implementations/Transactions/StockTransferRequestIntegration.cs
@@ -319,7 +319,6 @@ internal class StockTransferRequestIntegration(
             } : null,
             custbody_dbti_transfer_category = new { id = dto.TransferCategory.Id },
             custbody_dbti_prepared_by = dto.PreparedById,
-            custbody_dbti_custom_approval_status = 2, // draft
             custbody_dbti_return_to_vendor = dto.TransferCategory.IsReturn && dto.Vendor != null ? new { id = dto.Vendor.Id.ToString() } : null,
             Department = new { id = "4" },
             Class = new { id = "1" },
