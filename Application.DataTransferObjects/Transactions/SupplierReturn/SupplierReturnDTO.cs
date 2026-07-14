be29ecf62c31b8d86faf7bef9840ad43ded4f315
tbsirupert <tbsi.rupert@gmail.com>
2026-07-14 09:34:40 +0800

    feat: add "created from" field to return to supplier

--- Application.DataTransferObjects/Transactions/SupplierReturn/SupplierReturnDTO.cs
+++ Application.DataTransferObjects/Transactions/SupplierReturn/SupplierReturnDTO.cs
@@ -12,6 +12,7 @@ public class SupplierReturnDTO
     public int Id { get; set; }
     public int? PreparedById { get; set; }
     public string PreparedBy { get; set; } = string.Empty;
+    public string CreatedFrom { get; set; } = string.Empty;
     public DateTime Date { get; set; }
     public VendorDTO? Vendor { get; set; } = null;
     public LocationDTO? Location { get; set; } = null;
