00bcb5a6c35ede0b7c8d84dd6e2ea75503a6b8c0
tbsirupert <tbsi.rupert@gmail.com>
2026-07-17 13:01:24 +0800

    feat: barcode scanning dialog, barcode store class

---
+++ Web.BlazorServer/Components/Pages/Transaction/Others/BarcodeScanning/BarcodeScannerDialog.razor.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Components;
+using Web.BlazorServer.Handlers.Repositories.Transaction.Receiving;
+using Web.BlazorServer.ViewModels.Transaction.Receiving;
+
+namespace Web.BlazorServer.Components.Pages.Transaction.Others.BarcodeScanning;
+
+public partial class BarcodeScannerDialog
+{
+    [Inject] IReceivingHandler receivingHandler { get; set; } = default!;
+    [Parameter][EditorRequired] public BarcodeStore BarcodeStore { get; set; }
+    [Parameter] public BarcodeVerifier? Verifier { get; set; }
+
+    private BarcodeVM barcode = new();
+    private HashSet<string> FailedBarcodesCache = new();
+
+    const string ALERT_BARCODE_NOT_FOUND = "Couldn't find data for barcode {0}";
+    const string ALERT_BARCODE_SUCCESS = "";
+    string HelperString = string.Empty;
+
+    bool IsLoadingData = false;
+
+    async Task BarcodeScanned(BarcodeVM barcode)
+    {
+        if (BarcodeStore.Contains(barcode)) 
+        {
+            BarcodeStore.AddBarcode(barcode);
+            HelperString = ALERT_BARCODE_SUCCESS;
+            return;
+        }
+        if (FailedBarcodesCache.Contains(barcode.Barcode))
+        {
+            HelperString = string.Format(ALERT_BARCODE_NOT_FOUND, barcode.Barcode);
+        }
+
+        IsLoadingData = true;
+        try
+        {
+            var barcodeData = await GetBarcodeData(barcode);
+            if (barcodeData is null)
+            {
+                HelperString = string.Format(ALERT_BARCODE_NOT_FOUND, barcode.Barcode);
+                FailedBarcodesCache.Add(barcode.Barcode);
+            }
+            else
+            {
+                if (Verifier is not null && !Verifier(barcode, out string reason))
+                {
+                    HelperString = reason;
+                }
+                else
+                {
+                    BarcodeStore.AddBarcode(barcodeData);
+                    HelperString = ALERT_BARCODE_SUCCESS;
+                }
+            }
+        }
+        catch (Exception ex) 
+        {
+            HelperString = ex.Message;
+        }
+        IsLoadingData = false;
+    }
+
+    public void Close(bool keepChanges)
+    {
+        DialogService.Close(keepChanges);
+    }
+
+    Task<BarcodeVM?> GetBarcodeData(BarcodeVM barcode) => receivingHandler.GetBarcodeData(barcode.Barcode);
+
+    public delegate bool BarcodeVerifier(BarcodeVM barcode, out string reason);
+}
---
+++ Web.BlazorServer/Components/Pages/Transaction/Others/BarcodeScanning/BarcodeStore.cs
@@ -0,0 +1,35 @@
+﻿using Mapster;
+using Web.BlazorServer.ViewModels.Others;
+using Web.BlazorServer.ViewModels.Transaction.Commons;
+using Web.BlazorServer.ViewModels.Transaction.Receiving;
+
+namespace Web.BlazorServer.Components.Pages.Transaction.Others.BarcodeScanning;
+
+public class BarcodeStore
+{
+    private Dictionary<string, BarcodeStoreItem> _items = new();
+
+    public void AddBarcode(BarcodeVM barcode)
+    {
+        if (!_items.ContainsKey(barcode.Barcode)) _items[barcode.Barcode] = new(barcode);
+
+        _items[barcode.Barcode].Count++;
+    }
+    public decimal CountItemQuantity(int itemId) => _items.Values
+        .Where(x => x.Item?.Id == itemId)
+        .Sum(x => x.Count * (x.UoM?.ConversionRate ?? 0));
+    public int GetBarcodeCount(BarcodeVM barcode) => GetBarcodeCount(barcode.Barcode);
+    public int GetBarcodeCount(string barcode) => _items[barcode]?.Count ?? 0;
+    public bool Contains(BarcodeVM barcode) => _items.ContainsKey(barcode.Barcode);
+    public bool Contains(string barcode) => _items.ContainsKey(barcode);
+    public void Clear() => _items.Clear();
+    public bool Any() => _items.Any();
+    public IEnumerable<BarcodeVM> Barcodes => _items.Values.Select(x => x.Barcode);
+    public class BarcodeStoreItem(BarcodeVM barcode)
+    {
+        public BarcodeVM Barcode { get; init; } = barcode;
+        public int Count { get; set; } = 0;
+        public ItemsVM? Item => Barcode.Item;
+        public ItemUnitVM? UoM => Barcode.UoM;
+    }
+}
