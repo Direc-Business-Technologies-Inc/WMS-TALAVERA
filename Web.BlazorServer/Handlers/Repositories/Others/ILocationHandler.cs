20e058c138cf08fa98fa32bf932788758d6cdfdc
tbsirupert <tbsi.rupert@gmail.com>
2026-07-30 14:51:05 +0800

    feat: add get current user locations

--- Web.BlazorServer/Handlers/Repositories/Others/ILocationHandler.cs
+++ Web.BlazorServer/Handlers/Repositories/Others/ILocationHandler.cs
@@ -6,6 +6,7 @@ namespace Web.BlazorServer.Handlers.Repositories.Others;
 public interface ILocationHandler
 {
     Task<(IEnumerable<LocationVM> Data, int Count)> GetLocationsAsync(DataGridIntent intent);
+    Task<(IEnumerable<LocationVM> Data, int Count)> GetCurrentUserLocationsAsync(DataGridIntent intent);
     Task<(IEnumerable<LocationVM> Data, int Count)> GetSublocationsOfLocationAsync(DataGridIntent intent, int location);
     Task<(IEnumerable<LocationVM> Data, int Count)> GetLocationsBySubsidiaryAsync(DataGridIntent intent, int subsidiaryId);
     Task<(IEnumerable<LocationBinVM> Data, int Count)> GetLocationBinsAsync(int locationId, DataGridIntent intent);
--- Web.BlazorServer/Program.cs
+++ Web.BlazorServer/Program.cs
@@ -46,8 +46,8 @@ builder.Services.AddBlazorServerHandlers(builder.Environment);
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.Cookie.SameSite = SameSiteMode.Strict;
-        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
         options.ExpireTimeSpan = TimeSpan.FromHours(8);
         options.SlidingExpiration = true;
         options.LoginPath = "/";
